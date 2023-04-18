553061a746bc99ac6195fd56acd13467f29953a9
Zeak218 <iggy.torres@yahoo.com>
2023-04-18 16:26:16 +0800

    Added history, previous and next button

--- script.js
+++ script.js
@@ -21,31 +21,20 @@ const winConditions = [
     [2, 4, 6]
 ]
 
+let moves = []
+let redoMoves = []
+let moveCounter = 0
+
 const circle = document.getElementById("circle")
 const cross = document.getElementById("cross")
 const frontPage = document.getElementById("frontPage")
-const choiceGo = document.getElementById("choiceGo")
 const gameContainer = document.getElementById("gameContainer")
 
-//Transitions from "Let's Go" message to gameboard
-function boardSet () {
-    choiceGo.classList.toggle("hidden");
-    gameContainer.classList.toggle("hidden");
-}
-
-//Transitions from choice page, to "Let's Go" message,then to game board after 2 seconds
+//Transitions from choice page, to to game board after 2 seconds
 function choicePicked () {
         frontPage.classList.toggle("hidden");
-        choiceGo.classList.toggle("hidden");
-        setTimeout(boardSet, 2000);
+        gameContainer.classList.toggle("hidden");
 }
-
-//Enables transition when clicking either circle or cross on initial page
-circle.addEventListener("click", choicePicked)
-cross.addEventListener("click", choicePicked)
-
-//Resets game when "Restart" button is clicked
-restartBtn.addEventListener("click", startGame)
 
 //Starts the game by resetting the board (removing Xs and Os)
 function startGame () {
@@ -62,14 +51,25 @@ function startGame () {
         cell.addEventListener("click", cellClick, { once:true })
     })
     //Hides winner message, and buttons 
-    afterGameElement.classList.add("hidden")
+    afterGameElement.classList.add("hidden");
+    //Clears record of moves and turn move counter
+    moves = [];
+    redoMoves = [];
+    moveCounter = 0;
+    previousBtn.disabled = false;
+    nextBtn.disabled = false;
 }
 
 //Records if X or O turn, and evaluates game progress (Win, Draw, Next Turn)
 function cellClick(e) {
     const cell = e.target
     const currentClass = player_circle_turn ? player_circle : player_cross
+    const cellIndex = e.target.getAttribute("id")
     placeSymbol (cell, currentClass);
+    moves.push([cellIndex, currentClass]);
+    console.log(moves)
+    moveCounter = moveCounter + 1;
+    console.log(moveCounter)
     if (checkWin(currentClass)) {
         endGame(false)
     }
@@ -116,7 +116,7 @@ function changeTurns () {
     player_circle_turn = !player_circle_turn
 }
 
-//Checks if either side has a winning combination
+//Checks if side has a winning combination
 function checkWin (currentClass) {
     return winConditions.some(combination => {
         return combination.every(index => {
@@ -125,13 +125,40 @@ function checkWin (currentClass) {
     })
 }
 
+function undoMove(){
+    if (moves.length > 0) {
+        moveCounter --;
+        let lastMoveArr = moves[moves.length-1];
+        let lastMoveIndex = lastMoveArr[0];
+        document.getElementById(lastMoveIndex).classList.remove("X", "O");
+        redoMoves.push(moves.pop());
+    }
+    else if (moves.length === 0) {
+        previousBtn.disabled = true;
+        nextBtn.disabled = false;
+    }
+}
+
+function redoMove(){
+    if (redoMoves.length > 0 ) {
+        moveCounter ++;
+        let nextMoveArr = redoMoves[redoMoves.length-1];
+        let nextMoveIndex = nextMoveArr[0];
+        let nextMoveSymbol = nextMoveArr[1];
+        document.getElementById(nextMoveIndex).classList.add(nextMoveSymbol)
+        moves.push(redoMoves.pop());
+    }
+    else if (redoMoves.length === 0) {
+        nextBtn.disabled = true;
+        previousBtn.disabled = false;
+    }
+}
+
+circle.addEventListener("click", choicePicked)
+cross.addEventListener("click", choicePicked)
+restartBtn.addEventListener("click", startGame)
+previousBtn.addEventListener("click", undoMove)
+nextBtn.addEventListener("click", redoMove)
+
 //Initalizes board
 startGame();
-
-let boardStatus = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-
-let moves = []
